13911c2ca2933c30985de4ec972235d3ef6526ca
Yishin <hank100101212@gmail.com>
2025-06-23 14:55:58 +0800

    劇情製作到第二幕進門後看到妻子倒在地上的對話內容

--- Assets/Script/TriggerHandler.cs
+++ Assets/Script/TriggerHandler.cs
@@ -1,18 +1,37 @@
 using UnityEngine;
+using System.Collections;
 
 public class TriggerHandler : MonoBehaviour
 {
     [Header("Dialogue(optional)")]
     [SerializeField] private string dialogueKnotName;
 
+    [SerializeField] private bool intervalTrigger = false;
+    [SerializeField] private float intervalTime = 1.0f;
+
+    private bool hasTriggered = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+
+        if (hasTriggered) return;
+        if (!other.CompareTag("Player")) return;
+
+        if (!string.IsNullOrEmpty(dialogueKnotName))
         {
-            if (!dialogueKnotName.Equals(""))
+            if (!intervalTrigger)
             {
                 GameEventsManager.Instance.dialogueEvents.EnterDialogue(dialogueKnotName);
             }
+            else
+            {
+                StartCoroutine(DelayedTrigger());
+            }
         }
+    }
+    private IEnumerator DelayedTrigger()
+    {
+        yield return new WaitForSeconds(intervalTime);
+        GameEventsManager.Instance.dialogueEvents.EnterDialogue(dialogueKnotName);
     }
 }
